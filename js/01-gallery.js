991a240e7a73cae92ea4bd2c1f951cd7b9827098
Volodymyr Titarenko <xsligvaderx@gmail.com>
2022-12-12 22:36:22 -0600

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const galleryPicture = evt.target;
+  const getOriginalPicture = galleryPicture.dataset.original;
+  console.log(getOriginalPicture);
+}
 
 console.log(galleryItems);
